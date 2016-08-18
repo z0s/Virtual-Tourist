395d24188592b9d558aee970c56724f47ad37da9
Zeus <masgar001@gmail.com>
2016-08-18 12:13:37 -0700

    feat: Update PinAnnotation

---
+++ PinAnnotation.swift
@@ -0,0 +1,15 @@
+//
+//  PinAnnotation.swift
+//  VirtualTourist
+//
+//  Created by IT on 8/17/16.
+//  Copyright © 2016 z0s. All rights reserved.
+//
+
+import MapKit
+
+class PinAnnotation: MKPointAnnotation {
+    
+    var pin: Pin?
+
+}
